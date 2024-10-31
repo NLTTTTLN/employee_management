67a9b5bd15aef78c1ff5abe7b60dadc4d082a8cb
NLTTTTLN <tatthang.03042003@gmail.com>
2024-10-31 19:39:58 +0700

    Update AdminController: 31/10

--- src/employee_management/controller/AdminController.java
+++ src/employee_management/controller/AdminController.java
@@ -15,6 +15,7 @@ import employee_management.bean.Account;
 import employee_management.service.AccountService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,14 +24,39 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/admin")
 public class AdminController{
+	
+	@Autowired
+    private AccountService accountService;
+	
 	// GET /home cho trang chính của admin
-	@RequestMapping(value="/home", method = RequestMethod.GET)
+	@RequestMapping(value="/dashboard", method = RequestMethod.GET)
 		public String showAdminHomePage() {
-		return "admin/home";
+		return "admin/dashboard";
 	}
 	// Redirect /admin về trang chính của admin
-	@RequestMapping("/admin")
+	@RequestMapping("/")
 	public String redirectToHome(RedirectAttributes attributes) {
-			return "redirect:/admin/home";
+			return "redirect:admin/dashboard";
 		}
+	 @RequestMapping(value="/dashboard-info",method = RequestMethod.GET)
+	    @ResponseBody
+	    public Map<String, Object> getDashboardData() {
+	        // Assuming these methods exist in your AccountService
+	        int managerCount = accountService.getManagerCount(); // Get manager count
+	        int employeeCount = accountService.getEmployeeCount(); // Get employee count
+	        List<String> managerActivities = accountService.getRecentManagerActivities(); // Recent manager activities
+	        List<String> employeeActivities = accountService.getRecentEmployeeActivities(); // Recent employee activities
+
+	        // Prepare response data
+	        Map<String, Object> dashboardData = new HashMap<>();
+	        dashboardData.put("managerCount", managerCount);
+	        dashboardData.put("employeeCount", employeeCount);
+	        dashboardData.put("managerActivities", managerActivities);
+	        dashboardData.put("employeeActivities", employeeActivities);
+	        
+	        System.out.println("Manager Count: " + managerCount);
+	        System.out.println("Employee Count: " + employeeCount);
+
+	        return dashboardData; // This will be returned as JSON
+	    }
 }
