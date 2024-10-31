af006a1c33255da02104fbd2df5a541fe19560e4
NLTTTTLN <tatthang.03042003@gmail.com>
2024-10-31 19:40:32 +0700

    Update LoginController: 31/10

--- src/employee_management/controller/LoginController.java
+++ src/employee_management/controller/LoginController.java
@@ -47,7 +47,7 @@ public class LoginController {
 
 	        response.put("success", true); // Authenticate thành công
 	        // Respone được server trả về tương ứng với role của account
-	        response.put("redirectUrl", "admin".equals(account.getRole()) ? "admin/home" : "/user/home");
+	        response.put("redirectUrl", "admin".equals(account.getRole()) ? "admin/dashboard" : "/user/home");
 	    } else {
 	        response.put("success", false); // Authenticate thất bại
 	    }
