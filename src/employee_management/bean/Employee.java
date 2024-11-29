0e2ed55824d95da6c7126ce43cbeec98279c2731
NLTTTTLN <tatthang.03042003@gmail.com>
2024-11-30 05:20:23 +0700

    Update Employee.java

--- src/employee_management/bean/Employee.java
+++ src/employee_management/bean/Employee.java
@@ -3,7 +3,13 @@ package employee_management.bean;
 public class Employee extends User {
     private String department;
     private double salary;
-
+    
+    
+ // Default constructor
+    public Employee() {
+        super();
+    }
+    
     // Constructor
     public Employee(String username, String name, String gender, java.sql.Date dob, String email, String phone_num, String address, String department, double salary) {
         super(username, name, gender, dob, email, phone_num, address); // Call the User constructor
