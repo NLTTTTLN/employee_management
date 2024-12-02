9119daa024463536d36f5e46af73c2654830adbe
NLTTTTLN <tatthang.03042003@gmail.com>
2024-12-02 20:39:04 +0700

    Update Employee.java

--- src/employee_management/bean/Employee.java
+++ src/employee_management/bean/Employee.java
@@ -1,8 +1,10 @@
 package employee_management.bean;
 
 public class Employee extends User {
+	private Integer employee_id;
     private String department;
     private double salary;
+    
     
     
  // Default constructor
@@ -11,10 +13,25 @@ public class Employee extends User {
     }
     
     // Constructor
+    public Employee(Integer employee_id,String username, String name, String gender, java.sql.Date dob, String email, String phone_num, String address, String department, double salary) {
+
+    	super(username, name, gender, dob, email, phone_num, address); // Call the User constructor
+        this.employee_id = employee_id;
+        this.department = department;
+        this.salary = salary;
+    }
     public Employee(String username, String name, String gender, java.sql.Date dob, String email, String phone_num, String address, String department, double salary) {
         super(username, name, gender, dob, email, phone_num, address); // Call the User constructor
         this.department = department;
         this.salary = salary;
+    }
+    
+    public int getEmployeeId() {
+        return employee_id;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employee_id = employeeId;
     }
 
 	public String getDepartment() {
